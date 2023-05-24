1b3139ec351ef0732acb82df38fea1ba9f07f4df
Nadav Eliash <nadav.eliash@gmail.com>
2023-05-24 15:36:55 +0300

    try fix issue with render

--- backend/server.js
+++ backend/server.js
@@ -99,8 +99,8 @@ app.delete('/api/toy/:toyId', (req, res) => {
 
 const port = process.env.PORT || 3030
 
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// })
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
